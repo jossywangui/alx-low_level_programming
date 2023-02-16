eb4f8e63678eff3d03ae4d5c6eb1911b75b73382
jossywangui <wanguijossy98@gmail.com>
2023-02-16 14:13:36 -0800

    push

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-* main - A program that uses funtion printf
-*Return: 0 (success)
-*/
+ * main - A program that uses printf
+ * Return: 0 (success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n")
