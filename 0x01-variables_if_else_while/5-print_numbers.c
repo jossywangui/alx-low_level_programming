cc1017d7f9a5fef967ee7d876adeb097ae2bf048
jossywangui <wanguijossy98@gmail.com>
2023-02-17 10:05:27 -0800

    push

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -function
+ *
+ * Description: prints all single digits
+ *
+ * Return: (0) success
+ */
+
+int main(void)
+{
+	for (a = 0; a <= 10; a++)
+		putchar (a);
+	putchar ("\n");
+	return (0);
+}
