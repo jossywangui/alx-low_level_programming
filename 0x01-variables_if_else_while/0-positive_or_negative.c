bca435d280da114f7d5c6a816847a758a959b5d2
jossywangui <wanguijossy98@gmail.com>
2023-02-17 08:50:22 -0800

    push

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,34 @@
-#include <stdio.h>
+/* library stdlib and time included */
+
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- * main - main code
- * Description: Get a random number
- * and if it is positive, negative, or zero
- * Return: 0
+ * main -function
+ *
+ * Description: prints a random number
+ *
+ * Return: returns (0) on success
  */
+
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
+
+	if (n < 0)
+	{
 		printf("%d is negative\n", n);
-	else 
-		printf("%d is zero\n", n);
-	Return (0);
+	} else if (n == 0)
+	{
+		printf("0 is zero\n");
+	} else
+	{
+		printf("%d is positive\n", n);
+	}
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -function
+ *
+ * Description: prints lower case alphabet
+ *
+ * Return: 0 when success
+ */
+
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 97; i < 123; i++)
+	{
+		c = i
+		putchar(c)
+	}
+	putchar('\n');
+
+	return (0);
+}
+
+
+
+ 
