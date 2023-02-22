b2c2609f6a6332cafa5afcaea41f61cdaf5163a9
jossywangui <wanguijossy98@gmail.com>
2023-02-21 17:54:59 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,6 +2,7 @@
 
 /**
  * main - Entry point
+ * putchar: print alphabets putchar
  * Return: Always 0
  */
 
@@ -9,16 +10,15 @@ int main(void)
 
 {
 
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	_putchar( '_' );
+	_putchar( 'p' );
+	_putchar( 'u' );
+	_putchar( 't' );
+	_putchar( 'c' );
+	_putchar( 'h' );
+	_putchar( 'a' );
+	_putchar( 'r' );
+	_putchar( '\n' );
 	return (0);
 
 }
-
