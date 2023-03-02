95c85ae235780d45fd512dbcee40dbc0130ae2ef
jossywangui <wanguijossy98@gmail.com>
2023-03-02 11:40:15 -0800

    push

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - void function
+ * @n: integer to be printed.
+ *
+ * Description: prints an integer
+ */
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
+}
